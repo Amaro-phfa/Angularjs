d6c28bbbce33932f2b8d67f8bec303928c909e97
AMARO <amaro.pedro.phfa@gmail.com>
2023-03-14 14:14:39 -0300

    Arquivos_1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { LoginComponent } from './views/login/login.component';
+import { ContatoComponent } from './views/contato/contato.component';
+const routes: Routes = [
+  {path:"login", component: LoginComponent},
+  {path:"contato", component: ContatoComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
